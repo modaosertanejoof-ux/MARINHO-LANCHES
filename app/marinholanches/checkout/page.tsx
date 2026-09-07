8803bd9c68caa341b4d6414288cc156d1be38875
modaosertanejoof-ux <modaosertanejo.of@gmail.com>
2026-09-07 12:20:59 -0300

    Delete app directory

--- app/marinholanches/checkout/page.tsx
+++
@@ -1,3 +0,0 @@
-import { MOCK_RESTAURANT } from '@/lib/store-data'
-import CheckoutClient from '../../[slug]/checkout/CheckoutClient'
-export default function Page(){ return <CheckoutClient restaurant={MOCK_RESTAURANT}/> }
