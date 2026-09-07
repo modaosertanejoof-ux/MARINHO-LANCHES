0ec5b99840c88fba9694bde8b527a2b4b419ca50
modaosertanejoof-ux <modaosertanejo.of@gmail.com>
2026-09-06 23:56:46 -0300

    Delete app/[slug] directory

--- app/[slug]/checkout/page.tsx
+++
@@ -1,8 +0,0 @@
-import { prisma } from '@/lib/db'
-import { MOCK_RESTAURANT } from '@/lib/store-data'
-import CheckoutClient from './CheckoutClient'
-export default async function Page({params}:{params:{slug:string}}){
-  let restaurant:any = MOCK_RESTAURANT
-  try{ const r=await prisma.restaurant.findUnique({where:{slug:params.slug}}); if(r) restaurant=r }catch{}
-  return <CheckoutClient restaurant={restaurant}/>
-}
